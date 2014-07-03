8d23876a476458ff4d6814304d6ef62e346d3d01
Andre Brutus <andre@wepay.com>
2014-07-02 17:52:48 -0700

    added capped array object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+// Creates an array that deletes older items (circular queue)
+function CappedArray(n) {
+    this._array = new Array;
+    this.maxlength = n;
+}
+
+CappedArray.prototype.add = function(x) {
+    this._array.push(x);
+    if(this._array.length == this.maxlength) {
+        this._array.shift();
+    }
+}
+
+var container = new CappedArray(10);
+var DEBUG = false;
 $(document).ready(function() {
     var ws = new WebSocket("ws://23.253.228.216:3232");
     var log = $("#log");
@@ -13,13 +28,19 @@ $(document).ready(function() {
     ws.onerror = ws.onclose;
     ws.onmessage = function (event) {
         var payload = JSON.parse(event.data.replace(/=>/g,":"));
-        console.log(payload);
         var inner_payload = JSON.parse(payload.message);
-        log.append("<span class='hostname'>" + inner_payload.syslog_hostname + " </span>");
-        log.append("<span class='timestamp'>" + inner_payload.syslog_timestamp + " </span>");
-        log.append(inner_payload.syslog_message);
-        log.append("</br>");
-        console.log(inner_payload);
+        var log_line = '';
+        log_line += "<span class='hostname'>" + inner_payload.syslog_hostname + " </span>";
+        log_line += "<span class='timestamp'>" + inner_payload.syslog_timestamp + " </span>";
+        log_line += inner_payload.syslog_message;
+        log_line += "</br>";
+        log.append(log_line);
+        container.add(log_line);
         $(window).scrollTop($("#footer").offset().top);
+        if(DEBUG) {
+            console.log(payload);
+            console.log(inner_payload);
+        }
     }
 });
+
